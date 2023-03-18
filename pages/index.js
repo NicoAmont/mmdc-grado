7408240ce2c261e7cddc49af6e138e215b557fea
NicoAmont <nicolas.amezquita.m@gmail.com>
2023-03-18 17:27:12 -0500

    template to start

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from "next/link"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,10 +15,26 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/MMDC.svg" />
       </Head>
-      <main className="">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+      <main className="w-screen h-auto bg-bgr flex justify-center">
+        <div className='max-w-7xl w-screen flex justify-center'>
+          <div className='w-11/12 h-screen'>
+            <div className="bg-bgr flex  2xl:flex-row items-center  text-base w-full h-16 2xl:h-28  2xl:px-5  justify-between max-w-7xl">
+
+                {/* Left */}
+                <Link  href="/" passHref>
+                  <div className="grid place-content-start relative ml-3">
+                      <svg width="113" height="45" viewBox="0 0 113 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <circle cx="20" cy="20" r="20" fill="#94856F"/>
+                          <circle cx="68" cy="34" r="11" fill="#94856F"/>
+                          <circle cx="107" cy="34" r="6" fill="#94856F"/>
+                          <path d="M10 16.0005L19.732 11.1345C19.8152 11.0929 19.907 11.0713 20 11.0713C20.093 11.0713 20.1848 11.0929 20.268 11.1345L30 16.0005M28 19.0005V27.0005C28 27.5309 27.7893 28.0396 27.4142 28.4147C27.0391 28.7898 26.5304 29.0005 26 29.0005H14C13.4696 29.0005 12.9609 28.7898 12.5858 28.4147C12.2107 28.0396 12 27.5309 12 27.0005V19.0005" stroke="#070707" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                      </svg>
+                  </div>
+                </Link>
+
+            </div>
+          </div>
+        </div>
       </main>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,55 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend:{
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        jello: 'jello 1s ease-in-out infinite'
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        jello: {        
+          '0%, 100%': { transform: 'scale3d(1, 1, 1)' },
+          '30%': { transform: 'scale3d(1.25, 0.75, 1)' },
+          '40%': { transform: 'scale3d(0.75, 1.25, 1)' },
+          '50%': { transform: 'scale3d(1.15, 0.85, 1)' },
+          '65%': { transform: 'scale3d(0.95, 1.05, 1)' },
+          '75%': { transform: 'scale3d(1.05, 0.95, 1)' },
+        }
+      }
+    },
+    colors: {
+      bgr: '#070707',
+      gold: '#94856F',
+      bgr2: '#CCCCCC'
+
+
+
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    fontFamily: {
+      heads:['Italiana'],
+      body:['Nunito']
+    },
+    
   },
   plugins: [],
 }
