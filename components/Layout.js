3ecd90e4a4fc481c4136066961303402a4895588
NicoAmont <nicolas.amezquita.m@gmail.com>
2023-03-18 20:19:08 -0500

    template banner

---
+++ components/Layout.js
@@ -0,0 +1,11 @@
+
+
+export default function Layout ( { children }) {
+    return (
+        <div className="overflow-hidden bg-bgr font-heads" >
+
+            {children }
+            
+        </div>
+    )
+}
