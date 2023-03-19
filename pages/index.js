1e39757e1ac035195642a50f71fb99a2143efb6a
NicoAmont <nicolas.amezquita.m@gmail.com>
2023-03-19 10:10:12 -0500

    finishin banner2

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,12 @@ export default function Home() {
     <>
       
       <main className="w-screen h-auto bg-bgr flex justify-center">
-        <div className='max-w-7xl w-screen flex  flex-col justify-center'>
+        <div className='relative max-w-7xl w-screen flex  flex-col justify-center items-center'>
           <div className='w-11/12 h-screen'>
-            <div className="bg-bgr flex  2xl:flex-row items-center  text-base w-full h-16 2xl:h-28  2xl:px-5  justify-between max-w-7xl">
+            <div className="bg-bgr flex  2xl:flex-row items-center  text-base w-full h-16  justify-between">
                 {/* Left */}
                 <Link  href="/" passHref>
-                  <div className="grid place-content-start relative ml-3">
+                  <div className="grid place-content-start  ml-3">
                       <svg width="113" height="45" viewBox="0 0 113 45" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <circle cx="20" cy="20" r="20" fill="#94856F"/>
                           <circle cx="68" cy="34" r="11" fill="#94856F"/>
@@ -26,20 +26,20 @@ export default function Home() {
                 </Link>
                 {/* Rigth */}
                 <Link  href="/" passHref>
-                  <div className="grid place-content-start relative ml-96">
+                  <div className="grid place-content-start  mr-3">
                     <h1 className='font-heads text-bgr2 text-2xl'>MMDC</h1>
                   </div>
                 </Link>
             </div>
             
-            <div>
+            <div className=''>
               <p className='absolute w-80 top-1/2 left-1/3 transform -translate-y-1/4 bottom-0 font-body text-xl text-bgr2 font-extralight tracking-widest -rotate-90'>María Mónica Díez Cruz</p>
               <img className='absolute w-80 top-1/2 left-1/3 transform -translate-y-1/2 bottom-0 filter-none'  src="/fotobanner1.png"/>
             </div>
-            <div className='animate-slide-left top-1/4 left-16 absolute border-gold border-r-4 border-t-4'>
+            <div className='animate-slide-left top-1/4 left-0 absolute border-gold border-r-4 border-t-4'>
               <img className='h-full'  src="/izqbanner.png" />
             </div>
-            <div className='animate-slide-right top-32 right-16 absolute border-gold border-l-4 border-t-4'>
+            <div className='animate-slide-right top-1/4 right-0 absolute border-gold border-l-4 border-t-4'>
               <img className='h-full'  src="/derbanner.png" />
             </div>
               
